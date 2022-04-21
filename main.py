1f20a6abef5210264bea9d1ba7aeca018d4eb668
Dave <diacovmusic@gmail.com>
2022-04-20 22:53:16 -0400

    got it to pull individually numbered tables

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # Define URL
-url = "https://game8.co/games/Pokemon-Legends-Arceus/archives/356251"
+url = "https://game8.co/games/Pokemon-Legends-Arceus/archives/356252"
 # Ask hosting server to fetch url
 requests.get(url)
 resp = requests.get(url)
@@ -13,3 +13,12 @@ if resp.status_code == 200:
     print ('OK!')
 else:
     print ('Boo!')
+
+pages = requests.get(url).text
+
+# parser-lxml = Change html to Python friendly format
+soup = BeautifulSoup(pages, 'lxml')
+
+table = soup.find_all('table', class_='a-table a-table a-table', style_='')[5]
+# element = soup.find_all_next('table')
+print(table)
